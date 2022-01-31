58dbdc2fefa38d0fa7a0b5e26eaee2b0f296df07
vainTimoti <[181113715@students.mikroskil.ac.id]>
2022-01-31 07:57:13 +0700

    penambahan component react

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Nav, NavLink } from "./NavbarElements";
+
+
+
+const Navbar = () => {
+  return (
+    <>
+
+
+  <Nav><NavLink to='/'>Portfolio</NavLink></Nav>
+    
+  
+  </>
+  );
+ 
+  
+};
+
+export default Navbar;
